34207d896b78af6aa6b0115ebeca84e7499e6c81
sj <kio8110iop@gmail.com>
2020-05-06 14:31:36 +0900

    index.js 충돌해결

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-<<<<<<< HEAD
+
 import Home from "@/views/Home.vue";
 import Signup from '@/views/auth/Signup.vue';
 import Graph from "../views/graphView/Graph.vue";
@@ -8,24 +8,13 @@ import MonthChart from "../components/graph/MonthChart.vue";
 import DayChart from "../components/graph/DayChart.vue";
 import New from "../views/graphView/New.vue";
 import Recommend from "../views/recommendView/Recommend.vue";
-import Detailed from '@/components/Detailed';
 import Review from "../views/reviewView/ReviewMain.vue";
 import Store from "../components/review/Store.vue";
-=======
-import Home from "../views/Home.vue";
-import Graph from "../views/Graph.vue";
-import Recommend from "../views/Recommend/Recommend.vue";
-import Review from "../views/Review.vue";
-import MonthChart from "../components/graph/MonthChart.vue";
-import DayChart from "../components/graph/DayChart.vue";
-import New from "../views/New.vue";
-import Signup from '@/views/auth/Signup';
 import Detailed from '@/views/Recommend/Detailed';
 import Noticeboard from '@/views/review_board/Noticeboard';
 import ReviewBoard from '@/views/review_board/ReviewBoard';
 import Recipe from '@/views/foot_recommend/Recipe';
 import FootRecommend from '@/views/foot_recommend/FootRecommend';
->>>>>>> 15cfcd5472376cfd016662b26cbc227507c3c9a2
 
 Vue.use(VueRouter);
 
@@ -53,10 +42,9 @@ const routes = [{
         path: '/signup',
         name: 'Signup',
         component: Signup
-<<<<<<< HEAD
-=======
-      },
-      {
+
+    },
+    {
         path: "/noticeboard",
         name: "noticeboard",
         component: Noticeboard
@@ -65,7 +53,6 @@ const routes = [{
         path: "/reviewboard",
         name: "reviewboard",
         component: ReviewBoard
->>>>>>> 15cfcd5472376cfd016662b26cbc227507c3c9a2
     },
     {
         path: "/graph",
@@ -113,8 +100,8 @@ const routes = [{
         path: '/recipe',
         name: 'Recipe',
         component: Recipe
-      },
-      {
+    },
+    {
         path: "/footrecommend",
         name: "FootRecommend",
         component: FootRecommend
